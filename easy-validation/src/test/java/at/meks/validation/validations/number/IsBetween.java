7b4fef820944ec24c92218cedae13c5c06f8c839
Markus Hager <meks77@gmail.com>
2020-06-11 23:18:32 +0200

    optimized dependencies, removed old code

--- easy-validation/src/test/java/at/meks/validation/validations/number/IsBetween.java
+++
@@ -1,9 +0,0 @@
-package at.meks.validation.validations.number;
-
-import at.meks.validation.Validation;
-
-@FunctionalInterface
-public interface IsBetween {
-
-    Validation<Comparable<Number>> isBetween(Number min, Number max);
-}
