9f6cf16d4e88b1ca0134f3133acf0b5a32783c99
Markus Hager <meks77@gmail.com>
2026-04-07 18:11:54 +0200

    improved bash script for sonar execution

--- mvn-with-sonar.sh
+++ mvn-with-sonar.sh
@@ -11,9 +11,5 @@
 #    		    </activation>
 #    	    </profile>
 # * maven in  in path
-# * JAVA_HOME is set to Java Jdk 11+
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 mvn -Dmaven.test.failure.ignore=false test
-
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 mvn sonar:sonar
